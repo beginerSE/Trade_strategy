276c8ffd8df7da21dec4cbe2717a0b81b866347a
零細SE <42573936+beginerSE@users.noreply.github.com>
2019-08-18 17:37:01 +0900

    Update backtest.py

--- backtest.py
+++ backtest.py
@@ -25,14 +25,16 @@ def cal_backtest(price,mterm1=12,mterm2=26,mterm3=5,momterm=6):
 cal_backtest(price,mterm1=12,mterm2=26,mterm3=5,momterm=6)[-1]
 
 
+
 # グリッドサーチ的な感じで最適場パラメーターを発見する
 returns=[]
 for i in range(2,21):
-    for r in range(2,21):
-        for e in range(5,26):
-            for f in range(6,31):
-                returns.append([cal_backtest(price,mterm1=i,mterm2=r,mterm3=int(e),momterm=int(f))[-1],i,r,e,f])
-                print(returns[-1])
+    for r in range(2,41):
+        if i<r:
+            for e in range(5,41):
+                for f in range(6,31):
+                    returns.append([cal_backtest(price,mterm1=i,mterm2=r,mterm3=int(e),momterm=int(f))[-1],i,r,e,f])
+                    print(returns[-1])
 
 backtest_data = pd.DataFrame(returns)
 backtest_data.columns=['returns','macd_short','macd_long','macd_diff','momentam']
